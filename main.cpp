ba65186f1eb53a911a1ff8aeaadf754feb9696d9
Yeji Kim <yejkim.yk@gmail.com>
2026-09-18 22:33:43 -0700

    update 3

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ const int DAYS = 30;
 void displayArray(const array<int, DAYS>& samples) { 
     cout << "\nSoil samples processed each day:\n"; 
     for (auto it = samples.cbegin(); it != samples.cend(); ++it) { 
-        cout << *it << " "; } 
+        cout << *it << " "; 
+    } 
     cout << endl; }
 
 // function to display the vector
@@ -25,7 +26,8 @@ void displayArray(const array<int, DAYS>& samples) {
 void displayVector(const vector<int>& samples) {
     cout << "\nSoil samples processed each day:\n";
     for (auto it = samples.cbegin(); it != samples.cend(); ++it) { 
-        cout << *it << " "; } 
+        cout << *it << " "; 
+    } 
     cout << endl; 
 }
 
@@ -73,6 +75,119 @@ int main() {
     // array::empty() 
     
     cout << "Is the array empty? ";
+    
+    if (samples.empty()) { 
+        cout << "Yes\n"; 
+    } 
+    else { cout << "No\n"; 
+    }
+
+    // array::front() 
+    
+    cout << "Samples processed on Day 1: " << samples.front() << endl; 
+    
+    // array::back() 
+    
+    cout << "Samples processed on Day 30: " << samples.back() << endl; 
+    
+    // array::at() 
+    
+    cout << "Samples processed on Day 6: " << samples.at(5) << endl; 
+    
+    // array::operator[] 
+    
+    cout << "Samples processed on Day 11: " << samples[10] << endl; 
+    
+    // array::data() 
+    
+    cout << "First value using data(): " << *samples.data() << endl;
+
+    // calc total samples
+
+    int totalSamples = 0;
+
+    for (auto it = samples.begin(); it != samples.end(); ++it) { 
+        totalSamples += *it; 
+    } 
+        
+    cout << "\nTotal soil samples processed: " << totalSamples << endl; 
+    
+    // calc average 
+    
+    double average = static_cast<double>(totalSamples) / samples.size(); 
+    cout << "Average samples processed per day: " << average << endl;
+
+    // find highest/lowest processing days
+
+    auto highest = max_element(samples.begin(), samples.end()); 
+    auto lowest = min_element(samples.begin(), samples.end()); 
+
+    cout << "Highest number of samples in one day: " << *highest << endl; 
+    cout << "Lowest number of samples in one day: " << *lowest << endl;
+
+    // find which day had most/least samples
+
+    int highestDay = (highest - samples.begin()) + 1; 
+    int lowestDay = (lowest - samples.begin()) + 1; 
+    
+    cout << "Highest processing day: Day " << highestDay << endl; 
+    cout << "Lowest processing day: Day " << lowestDay << endl;
+
+    // find how many days were above avg
+
+    int aboveAverage = 0; 
+    for (auto it = samples.begin(); it != samples.end(); ++it) { 
+        if (*it > average) { 
+            aboveAverage++; 
+        } 
+    } 
+        
+        cout << "Days above the average: " << aboveAverage << endl;
+
+    // std::vector
+    
+    cout << "PART 2: STD::VECTOR\n"; 
+
+    //make an empty vector
+    vector<int> sampleVector;
+
+    // open ext data file
+    ifstream inputFile("soil_samples.txt");
+
+    if (!inputFile) {
+        cout << "Error opening file\n";
+        return 1;
+    }
+
+    // read data into vector
+    int sampleCount; 
+
+    while (vectorFile >> sampleCount) { 
+        sampleVector.push_back(sampleCount); 
+    } 
+    
+    vectorFile.close();
+
+    // display vector
+    displayVector(sampleVector);
+
+    //vector functions:
+
+    // vector::size() 
+    cout << "\nNumber of days recorded: " << sampleVector.size() << endl; 
+    
+    // vector::capacity() 
+    cout << "Vector capacity: " << sampleVector.capacity() << endl; 
+    
+    // vector::empty() 
+    cout << "Is the vector empty? "; 
+    
+    if (sampleVector.empty()) { 
+        cout << "Yes\n"; 
+    } 
+    else { 
+        cout << "No\n"; 
+    }
 
     return 0;
 }
